71855a7948bbdce7babad4e5800b3822aea21749
TylerBeach <TylerAllenBeach@gmail.com>
2024-04-21 23:04:24 -0600

    fix overflow bug

--- frontend/src/Components/SquareCardContainer.tsx
+++ frontend/src/Components/SquareCardContainer.tsx
@@ -7,12 +7,12 @@ function SquareCardContainer() {
   return (
     <div>
         <h1 className="text-white text-4xl my-3">Projects</h1>
-        <div className='flex flex-row flex-wrap gap-x-6 w-[100%] overflow-auto'>
+        <div className='flex flex-row flex-wrap gap-x-6 w-[100%] h-min overflow-y-hidden'>
             {ProjectData.projects.slice(0, 7).map((project, index) => (
                 <motion.div key={project.id} initial="hidden" animate="visible" variants={{
                     hidden: { opacity: 0, y: 100 },
                     visible: { opacity: 1, y: 0, 
-                    transition: { delay: (2.2 + index * 0.1) } } }}>
+                    transition: { delay: (2 + index * 0.1) } } }}>
                         <Link href={`/projects/${project.id}`} passHref>
                     <div key={index} className='hover-effect card_background p-2.5 gap-y-1 flex flex-col w-[180px] h-auto rounded-md text-white'>
                             <img className='w-[100%] h-[160px] rounded-md' src={project.imageURL} alt={project.title} />

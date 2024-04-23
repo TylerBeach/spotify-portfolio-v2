ddceab44d30af105ae12442b2a2f8fd8247cc459
TylerBeach <TylerAllenBeach@gmail.com>
2024-04-23 02:47:33 -0600

    pad project items + reduce gradient height

--- frontend/src/Components/Project.tsx
+++ frontend/src/Components/Project.tsx
@@ -36,7 +36,7 @@ interface Project {
 
     return (
         <div className='card_background text-white rounded-md font-SpotifyMedium max-w-[100vw]'>
-            
+
             {/* image banner + title and date */}
             <div  style={{ backgroundImage: `url(${project.bannerURL})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'right' }} 
                 className='flex flex-col justify-between w-full h-[400px]'>
@@ -52,14 +52,14 @@ interface Project {
             {/* gradient color background from image banner */}
             <div style={{
                 background: `linear-gradient(to bottom, rgb(${bannerColor}) 0%, rgb(18, 18, 18) 100%)`,
-                height: "300px",
+                height: "200px",
                 width: "100%"
-                    }}
+            }}
             >
             </div>
 
             {/* project paragraphs */}
-            <div className='flex flex-col w-full mt-[-200px]'>
+            <div className='px-4 flex flex-col w-full mt-[-100px]'>
                 {project.paragraphData.map((aParagraph, index) => (
                     <div key={index} className='flex flex-col mb-4'>
                         <h2 className='text-2xl'>{aParagraph.paragraphTitle}</h2>
@@ -69,26 +69,27 @@ interface Project {
             </div>
             
             {/* project images */}
-            <div className='flex flex-row gap-3 flex-wrap gap-x-4 lg:justify-evenly'>
+            <div className='px-4 flex flex-row gap-3 flex-wrap gap-x-4 lg:justify-evenly'>
                 {project.demoImages.map((aDemoImage, index) => (
                     <img className='h-auto w-[100%] md:min-w-[500px] lg:w-[40%] lg:min-w-[600px]' src={aDemoImage} alt="Demo Image" key={index} />
                 ))}
             </div>
 
             {/* project tech stack */}
-            <div className='flex flex-col'>
-                
+            <div className='px-4 flex flex-col my-4'>
+                <h2 className='text-2xl'>Tech Stack</h2>
+
                 {project.techStack.map((techStack, index) => (
-                    <div key={index} className='flex flex-row rounded-md w-[100%] h-auto min-w-[300px] gap-x-6 items-center SpotifyLightFont'>
+                    <div key={index} className='hover-effect flex flex-row rounded-md w-[100%] h-auto min-w-[300px] gap-x-6 pl-6 items-center SpotifyLightFont'>
                         <h2 className='text-gray-400 w-2'>{index}</h2>
                         <img className='rounded-md w-[50px] h-[50px] object-contain m-2 ' src={`/images/techStack/${techStack}.png`} alt="Tech Stack"/>
                         <h1 className='text-white font-semibold text-xl'>{techStack}</h1>
                     </div>
                 ))}
             </div>
-
+                
             {/* project github link */}
-            <a href={project.link}>Link</a>
+            <a href={project.link} className='px-4'>Link</a>
         </div>
     );
 }

8c30c2440e61d664a524101c877eee6cccbb99a6
TylerBeach <TylerAllenBeach@gmail.com>
2024-04-26 23:18:49 -0600

    fix about page

--- frontend/src/Components/ProfileSongsContainer.tsx
+++ frontend/src/Components/ProfileSongsContainer.tsx
@@ -9,9 +9,9 @@ function ProfileSongsContainer() {
     <div className='flex flex-col gap-y-4 text-white'>
         <h1 className='text-2xl'>Popular</h1>
         
-        <div className='flex flex-col gap-y-4 w-full h-fit text-white ml-4'>
+        <div className='flex flex-col gap-y-2 w-full h-fit text-white ml-4 mr-4'>
             {aboutData.paragraphs.map((paragraph, index) => (
-                <div key={index} className='flex flex-row rounded-md w-[100%] h-[50px] min-w-[300px] gap-x-6 items-center SpotifyLightFont'>
+                <div key={index} className='flex flex-row rounded-md w-[95%] min-w-[300px] gap-x-6 items-center SpotifyLightFont hover-effect2 p-2'>
                     <h2 className='text-gray-400 w-2'>{index+1}</h2>
                     <img className='rounded-md w-[50px] h-[50px] object-cover' src={paragraph.image} alt={paragraph.title}/>
                     <h1 className='text-white font-semibold text-xl'>{paragraph.title}</h1>

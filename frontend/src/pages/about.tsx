e7384fdf0b93380db356795096403e199f8a6d1d
TylerBeach <TylerAllenBeach@gmail.com>
2024-05-06 20:46:28 -0600

    comments

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,27 +6,28 @@ import ProfileSongsContainer from "@/Components/ProfileSongsContainer";
 import MusicPlayer from "@/Components/MusicPlayer";
 import { motion } from "framer-motion";
 import ProfileTopContainer from "@/Components/ProfileTopContainer";
-import { useState, useEffect } from "react"; 
+import { useState, useEffect } from "react";
 import getSecondDominantColor from "@/utils/getDominantColor";
-import data from "../data.json"
-import {Project } from "../interfaces/project";
-
+import data from "../data.json";
+import { Project } from "../interfaces/project";
 
 function about() {
   const aboutMeProject: Project = {
     id: "aboutMeProject",
-    title: "Tyler Beach", };
+    title: "Tyler Beach",
+  };
   const [bannerColor, setBannerColor] = useState("");
 
   useEffect(() => {
-    getSecondDominantColor(data.about.bannerURL, (averageColorHex:any) => {
-        console.log('Average Color in Hex:', averageColorHex);
-        setBannerColor(averageColorHex);
+    getSecondDominantColor(data.about.bannerURL, (averageColorHex: any) => {
+      console.log("Average Color in Hex:", averageColorHex);
+      setBannerColor(averageColorHex);
     });
   }, []);
 
   return (
-      <div className="flex flex-row gap-x-2 bg-black max-w-[100vw] pb-[90px] min-h-[100vh] max-h-[100vh] md:pl-2 overflow-hidden">
+    <div className="flex flex-row gap-x-2 bg-black max-w-[100vw] pb-[90px] min-h-[100vh] max-h-[100vh] md:pl-2 overflow-hidden">
+      {/* Navbar Container */}
       <motion.div
         initial="hidden"
         animate="visible"
@@ -38,38 +39,39 @@ function about() {
       >
         <Navbar />
       </motion.div>
-        <motion.div
-          className="relative mt-0 md:mt-2 h-fit height-minus-musicPlayer w-full  overflow-y-scroll"
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: { opacity: 0, y: 200 },
-            visible: { opacity: 1, y: 0, transition: { delay: 1 } },
-          }}
-        >
-          <div className="flex flex-col h-fit w-full pt-0 card_background rounded-md  min-h-[100vh] overflow-x-hidden">
-            <TopNavButtons bannerColor={bannerColor} project={aboutMeProject}/>
-            <div className="rounded-md card_background h-fit w-full">
-              <ProfileTopContainer />
-              <div style={{
-                  background: `linear-gradient(to bottom, rgb(${bannerColor}) 0%, rgb(18, 18, 18) 100%)`,
-                  height: "200px",
-                  width: "100%"
-              }}
-              ></div>
-              <div className="mt-[-100px] flex flex-col h-fit z-[2] pl-4 gap-y-4">
-                <ProfileSongsContainer />
-                <ProfileSongsContainer />
-                <ProfileSongsContainer />
-              </div>
 
+      {/* Page Content Container */}
+      <motion.div
+        className="relative mt-0 md:mt-2 h-fit height-minus-musicPlayer w-full  overflow-y-scroll"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: { opacity: 0, y: 200 },
+          visible: { opacity: 1, y: 0, transition: { delay: 1 } },
+        }}
+      >
+        <div className="flex flex-col h-fit w-full pt-0 card_background rounded-md  min-h-[100vh] overflow-x-hidden">
+          <TopNavButtons bannerColor={bannerColor} project={aboutMeProject} />
+          <div className="rounded-md card_background h-fit w-full">
+            <ProfileTopContainer />
+            <div
+              style={{
+                background: `linear-gradient(to bottom, rgb(${bannerColor}) 0%, rgb(18, 18, 18) 100%)`,
+                height: "200px",
+                width: "100%",
+              }}
+            ></div>
+            <div className="mt-[-100px] flex flex-col h-fit z-[2] pl-4 gap-y-4">
+              <ProfileSongsContainer />
             </div>
           </div>
-        </motion.div>
-        <MusicPlayer />
-      </div>
+        </div>
+      </motion.div>
+      
+      {/* Music player at bottom of screen */}
+      <MusicPlayer />
+    </div>
   );
 }
-// TODO: make the scroll bar not scroll over the music player
 
 export default about;

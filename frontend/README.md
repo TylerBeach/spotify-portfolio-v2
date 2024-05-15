3f8e34d10c64be2128f5ea5ebd9c423e414267f4
Tyler Beach <tabeach@ualberta.ca>
2024-05-15 17:20:16 -0600

    update readme goals + init search page

--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,9 @@
 # TODO 
 - Convert the rectangle cards into about me information 
 - Add Projects to data.json *in progress*
-- Picture displayer for images inside a project page
+- Picture displayer for images inside a project page *done*
 - Make left nav have recently clicked queue 
 - Randomize bannners for index page 
+- Add search feature for entire site
 - Make musicPlayer responsive *done*
 - Make top nav change opacity with scroll *done*
---
+++ frontend/src/pages/search.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Navbar from '@/Components/Navbar'
+import MusicPlayer from '@/Components/MusicPlayer'
+import TopNavButtons from '@/Components/TopNavButtons'
+
+export default function search() {
+  return (
+    <main className="flex flex-row gap-x-2 bg-black max-w-[100vw] pb-[90px] min-h-[100vh] max-h-[100vh] md:pl-2 overflow-hidden">
+    {/* Navbar Container */}
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0, y: 200 },
+        visible: { opacity: 1, y: 0, transition: { delay: 1 } },
+      }}
+      className="hidden md:flex w-[300px] md:max-w-[300px] md:min-w-[300px] mt-2"
+    >
+      <Navbar />
+    </motion.div>
+
+    <motion.div
+      className="relative mt-0 md:mt-2 h-fit height-minus-musicPlayer w-full  overflow-y-scroll"
+      initial="hidden"
+      animate="visible"
+      variants={{
+        hidden: { opacity: 0, y: 200 },
+        visible: { opacity: 1, y: 0, transition: { delay: 1 } },
+      }}
+    >
+      <div className="flex flex-col h-fit w-full pt-0 card_background rounded-md  min-h-[100vh] overflow-x-hidden">
+        <TopNavButtons bannerColor={null} project={null} />
+          
+      </div>
+    </motion.div>
+    <MusicPlayer />
+  </main>
+  )
+}

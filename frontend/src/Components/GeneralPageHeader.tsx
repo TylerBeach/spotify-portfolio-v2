7191bec09a754fc0f0a5ca1f49028d4275c341cd
TylerBeach <TylerAllenBeach@gmail.com>
2024-05-24 03:06:47 -0600

    convert projectpage header to use generalHeader for consistency

--- frontend/src/Components/GeneralPageHeader.tsx
+++ frontend/src/Components/GeneralPageHeader.tsx
@@ -14,7 +14,7 @@ export default function PageHeader({title, subHeading, imageURL} : {title: strin
     return (
     <div className='w-full h-auto'>
         <div style={{ backgroundColor: `rgb(${bannerColor})`}} className='w-full h-fit pt-[75px] p-5 gap-x-6  flex flex-row content-baseline align-baseline'>
-            <img src={imageURL} alt="Projects" className='w-[20%] min-w-[100px] h-auto rounded-md max-w-[250px]'/>
+            <img src={imageURL} alt="Projects" className='w-[20%] min-w-[100px] h-auto rounded-md max-w-[250px] aspect-square '/>
             <div className='w-[80%] flex flex-col justify-items-end justify-end'>
                 <h1 className='text-white text-4xl md:text-5xl lg:text-7xl text-nowrap'>{title}</h1>
                 <p className='pl-1 text-white text-md SpotifyLightFont'>{subHeading}</p>
--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -10,6 +10,7 @@ import ProjectListView from "@/Components/ProjectListView";
 import ProjectsPageHeader from "@/Components/ProjectsPageHeader";
 import { Project } from "../interfaces/project";
 import getSecondDominantColor from "@/utils/getDominantColor";
+import GeneralPageHeader from "@/Components/GeneralPageHeader";
 
 function about() {
 
@@ -29,7 +30,7 @@ function about() {
       </motion.div>
 
       <motion.div
-        className="relative mt-0 md:mt-2 h-fit height-minus-musicPlayer w-full  overflow-y-scroll"
+        className="relative mt-0 md:mt-2 h-fit height-minus-musicPlayer w-full  overflow-hidden"
         initial="hidden"
         animate="visible"
         variants={{
@@ -37,9 +38,9 @@ function about() {
           visible: { opacity: 1, y: 0, transition: { delay: 1 } },
         }}
       >
-        <div className="flex flex-col h-fit w-full pt-0 card_background rounded-md  min-h-[100vh] overflow-x-hidden">
+        <div className="flex flex-col h-fit w-full pt-0 card_background rounded-md height-minus-musicPlayer overflow-y-auto overflow-x-hidden">
           <TopNavButtons imageURL={"/images/ProjectPage.png"} title="Projects" />
-            <ProjectsPageHeader />
+            <GeneralPageHeader title="Projects" subHeading="Tyler Beach Projects" imageURL="/images/ProjectPage.png" />
           <div className="mt-[-150px]">
             <ProjectListView />
           </div>

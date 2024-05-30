300f7285a6f031144f28f86df3fb34f79b94fc48
TylerBeach <TylerAllenBeach@gmail.com>
2024-05-30 15:26:46 -0600

    potential phone fix

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
           <title>{`Tyler's Portfolio`}</title>

ab063e5d0d465d24c28624b3f92f5741f317d5b2
TylerBeach <TylerAllenBeach@gmail.com>
2024-05-30 10:17:33 -0600

    fix margin error in navbar in about

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -54,7 +54,7 @@ function About() {
             hidden: { opacity: 0, y: 200 },
             visible: { opacity: 1, y: 0, transition: { delay: 0.5 } },
           }}
-          className="hidden md:flex w-[300px] md:max-w-[300px] md:min-w-[300px] mt-2"
+          className="hidden md:flex w-[300px] md:max-w-[300px] md:min-w-[300px]"
         >
           <Navbar />
         </motion.div>
